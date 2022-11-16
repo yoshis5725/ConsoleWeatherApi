a465b8e8032b6936c2451e7943e683b38cdfb6b2
elliottpolite <elliottp@ats-companies.com>
2022-11-15 18:14:02 -0700

    Completed Module 13 - Console Weather Api

---
+++ WeatherApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+
+var apiKey = "c43d574b3e4d08bbd37fe5f80ffeb393";
+var httpClient = new HttpClient();
+
+Console.WriteLine("Enter City: ");
+var cityName = Console.ReadLine();
+
+var directGeoCodingUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+var response = httpClient.GetStringAsync(directGeoCodingUrl).Result;
+
+var formattedResponse = JObject.Parse(response).GetValue("main")?.ToString();
+var temp = JObject.Parse(formattedResponse).GetValue("temp").ToString();
+var feelsLike = JObject.Parse(formattedResponse).GetValue("feels_like").ToString();
+
+Console.WriteLine(cityName);
+if (formattedResponse != null)
+{
+    Console.WriteLine($"Temperature: {temp}f");
+    Console.WriteLine(($"Feels Like: {feelsLike}f"));
+}
